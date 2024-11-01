54da8c954db4da25c7d3033246fa40879552b7c7
paulon-zervoulakus <paulon.zervoulakus@gmail.com>
2024-11-01 15:50:46 +0800

    fix cache issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-pau-[hash].js',
+        chunkFileNames: 'assets/[name]-pau-[hash].js',
+        assetFileNames: 'assets/[name]-pau-[hash][extname]'
+      }
+    }
+  }
 })
